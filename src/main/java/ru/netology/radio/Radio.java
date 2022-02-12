a198930b9134c2b7e98964de5ee543b287ab929d
nickola102 <nickola@list.ru>
2022-02-12 11:18:02 +0300

    add all tests

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,53 @@
+package ru.netology.radio;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() { return currentStation; }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation > 9) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void setNextStation() {
+        if (currentStation == 9) {
+            currentStation = 0;
+        } else {
+            currentStation++;
+        }
+    }
+
+    public void setPrevStation() {
+        if (currentStation == 0) {
+            currentStation = 9;
+        } else {
+            currentStation--;
+        }
+    }
+
+    public int getCurrentVolume() { return currentVolume; }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume > 10) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void makeLouder() {
+        if (currentVolume < 10) {
+            currentVolume++;
+
+        }
+    }
+
+    public void makeQuieter() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+}
